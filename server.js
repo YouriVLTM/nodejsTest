54bfb38549dfef606465f1347cf44ca228f0e0ad
YouriVLTM <r0694461@student.thomasmore.be>
2020-06-09 21:02:20 +0200

    ss

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express();
 
 
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+//socket json file
+app.use(function(req, res, next) {
+       res.header("Access-Control-Allow-Origin", "*");
+       res.header("Access-Control-Allow-Headers", "X-Requested-With");
+       res.header("Access-Control-Allow-Headers", "Content-Type");
+       res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+       next();
+   });
+
+
+   
 app.get('/', (req, res) => {
   res.json({"message": "TEST"});
 });
