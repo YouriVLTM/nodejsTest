a59ad94d52350ad7bc74a0262e71ce33c7fb54ce
YouriVLTM <r0694461@student.thomasmore.be>
2020-06-09 20:44:19 +0200

    ok
    ok

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-const http = require('http');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(process.env.PORT || 8080, ()=>console.log("ok"));
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
